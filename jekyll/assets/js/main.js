43f1ef6ed154fd735eaab97f0c7deec39d500422
Morgan Mikel McDaris <morganmcdaris@gmail.com>
2016-10-11 14:29:45 -0700

    Add 'Contact Us' link to the header

--- jekyll/assets/js/main.js
+++ jekyll/assets/js/main.js
@@ -3,19 +3,28 @@
 
 $( document ).ready(function() {
 
-	// Allow navigation to slide open and close on small devices
-	$("#nav-button").click(function(){
-		event.preventDefault();
+  // Allow navigation to slide open and close on small devices
+  $("#nav-button").click(function() {
+    event.preventDefault();
 
-		$("#nav-button").toggleClass("open");
-		$("nav.sidebar").toggleClass("open");
-	});
+    $("#nav-button").toggleClass("open");
+    $("nav.sidebar").toggleClass("open");
+  });
 
-	// Give article headings direct links to anchors
-	$("article h2, article h3, article h4, article h5, article h6").filter("[id]").each(function () {
-		$(this).append('<a href="#' + $(this).attr("id") + '"><i class="fa fa-link" style="display: none;"></i></a>');
-	});
-	$("article h2, article h3, article h4, article h5, article h6").filter("[id]").hover(function () {
-		$(this).find("i").toggle();
-	});
+  // open intercom when the contact us link is clicked
+  $("#contact-us").click(function() {
+    var intercom = $("#intercom-container");
+    if (intercom != null) {
+      event.preventDefault();
+      Intercom('show');
+    }
+  });
+
+  // Give article headings direct links to anchors
+  $("article h2, article h3, article h4, article h5, article h6").filter("[id]").each(function () {
+    $(this).append('<a href="#' + $(this).attr("id") + '"><i class="fa fa-link" style="display: none;"></i></a>');
+  });
+  $("article h2, article h3, article h4, article h5, article h6").filter("[id]").hover(function () {
+    $(this).find("i").toggle();
+  });
 });
